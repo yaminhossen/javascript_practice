6c80ae8ec60cbc1007d823d29167aed3e3ea2f45
yaminhossen <streatdot616@gmail.com>
2024-11-01 21:54:30 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,91 @@
+// find the second largest number
+// let numbers = [6,2,3,9,1,4];
+/* function bignum(numbers) {
+    let aa = numbers.sort();
+    return aa[aa.length - 2];
+    
+}
+console.log(bignum(numbers)); */
+
+// sortin assending order
+
+/* console.log(numbers.sort(function (a, b) {
+    return a-b;
+})); */
+
+// sortin desending order
+
+/* console.log(numbers.sort(function (a, b) {
+    return b-a;
+})); */
+
+
+// vowel count form an array
+
+/* const vowel = ["a", "e", "i", "o", "u"];
+
+function countVowel(arr) {
+    let count = 0;
+    let lowcase = arr.toLowerCase();
+    let letters = Array.from(lowcase);
+    // let letters = [...arr];
+    // console.log(letters);
+    letters.forEach(function (value) {
+        if(vowel.includes(value)){
+            count++;
+        }
+    })
+    // console.log('count', count);
+    return count;
+    
+    
+}
+countVowel("aeiouAe") */
+
+// finding duplicate number from an array
+
+/* const numbers = [2, 3, 4, 2, 5, 6, 5];
+function findDuplicate(arr) {
+    let dupArray = arr.filter((n, index) => index !== arr.indexOf(n))
+    return dupArray;
+}
+console.log(findDuplicate(numbers)); */
+
+// finding unique number from an array
+
+/* const numbers = [2, 3, 4, 2, 5, 6, 5];
+function findDuplicate(arr) {
+    let dupArray = arr.filter((n, index) => index === arr.indexOf(n))
+    return dupArray;
+}
+console.log(findDuplicate(numbers));
+ */  
+
+// finding how much time yamin in this array?
+
+/* const sentense = "yamin how are you yamin can yeo talk yamin";
+function countName(sent) {
+    let arr = sent.split(" ");
+    let countarr = [];
+    arr.forEach((e, index) => {
+        if(e == 'yamin'){
+            countarr.push(e)
+        }
+    });
+    return {countarr};
+}
+console.log(countName(sentense), countName(sentense).countarr.length); */
+
+
+// if the element fist time found then the function can not continue
+
+/* function linearsearch(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === val){
+            return i;
+        }
+        
+    }
+    return 'not found!';
+}
+console.log(linearsearch([2, 4, 3, 5, 4], 4)); */
---
+++ intro.py
@@ -0,0 +1,5 @@
+1. html
+2.css
+3. javascript
+4. database ( postgresql)
+5. cyber security
